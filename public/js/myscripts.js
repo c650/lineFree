0407918b7ea3053deeab8a2ec02ebbd354ff528b
gianlucazigni <gianlucazigni@gmail.com>
2015-07-16 10:13:36 -0400

    added split view on large devices for index places and posts

--- public/js/myscripts.js
+++ public/js/myscripts.js
@@ -1,15 +1,17 @@
 	$(document).ready(function() {
       $('select').material_select();
       $(".button-collapse").sideNav();
+      $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
   	});
+
   	$(".dropdown-button").dropdown();
   	$('.fixed-action-btn').openFAB();
   	$('.fixed-action-btn').closeFAB();
 
-  $('.datepicker').pickadate({
-      selectMonths: true, // Creates a dropdown to control month
-      selectYears: 15 // Creates a dropdown of 15 years to control year
-  });
+  
 
   $('.button-collapse').sideNav({
       menuWidth: 300, // Default is 240
